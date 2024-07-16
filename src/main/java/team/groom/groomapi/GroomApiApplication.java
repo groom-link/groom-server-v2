f666cf6134258d0da3f05a91d93accc7537b069f
jsween5723 <jsween5723@gmail.com>
2024-07-16 18:59:36 +0900

    feat(init): 프로젝트 초기화

---
+++ src/main/java/team/groom/groomapi/GroomApiApplication.java
@@ -0,0 +1,13 @@
+package team.groom.groomapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GroomApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/team/groom/groomapi/GroomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package team.groom.groomapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
